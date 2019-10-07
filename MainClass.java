1b3fcc9d3ba2345375a3a21a31be42fd2aff3879
rhodgeson <55752201+rhodgeson@users.noreply.github.com>
2019-10-06 21:54:32 -0500

    added filewriter

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MainClass {
@@ -8,19 +10,28 @@ public class MainClass {
         int maxsize = 50;
         int k = 2;
         MinHeap A = new MinHeap(maxsize, k);
-        try ( Scanner fin = new Scanner(new File("karyHeap-input.txt"))) {
+        try (Scanner fin = new Scanner(new File("karyHeap-input.txt"))) {
+            FileWriter write = new FileWriter(new File("output.txt"));
+            Scanner in;
+            int i = 0;
             while (fin.hasNextLine()) {
-                Scanner in = new Scanner(fin.nextLine());
+                in = new Scanner(fin.nextLine());
                 String choice = in.next();
                 if (choice.equals("IN")) {
                     A.insert(in.nextInt());
                 }
-                else
-                    A.extractMin();
+                else {
+                    i++;
+                    write.write(A.extractMin() + " \\ result for extract-min operation " + i+"\n");
+                }
             }
+            write.close();
         }
         catch (FileNotFoundException e) {
             System.out.println("There was no file found with the name karyHeap-input.txt");
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
         }
         long endTime = System.nanoTime();
         long timeElapsed = endTime - startTime;
